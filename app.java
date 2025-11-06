1752734f0a9fcfae71351ff74ed2190e0ac5e7e0
root <root@ip-172-31-21-240.ec2.internal>
2025-11-06 02:20:48 +0000

    change

--- app.java
+++ app.java
@@ -4,7 +4,13 @@ this is java file
 
 we are learning git
 
+<<<<<<< Updated upstream
 adding some code to check error 
 
 
   trouleshooting
+=======
+adding some code to check error
+
+error status
+>>>>>>> Stashed changes
